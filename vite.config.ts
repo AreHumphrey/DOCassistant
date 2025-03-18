75fb2755f1ac48b9ac024c366c037229330d368a
Vinnitckaya Dina <vinnitckaya.ds@dvfu.ru>
2025-03-18 21:44:48 +1000

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths';
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: {
+    open: true, // Автоматическое открытие браузера
     proxy: {
       // Проксируем запросы с /api на бэкенд
       '/api': {
-        target: 'http://localhost:8000', // Адрес вашего бэкенда
+        target: 'https://doc-assistant.ai/', // Адрес вашего бэкенда
         changeOrigin: true, // Подменяет Origin заголовка запроса на адрес бэкенда
         secure: false, // Если используете HTTPS, но сертификат невалидный
       },
