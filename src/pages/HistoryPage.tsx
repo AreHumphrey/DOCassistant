e5a10cc705583fb4f1f93ddfa6945778dcaab1b5
AreHumphrey <ajjbdfnhfjkvn@gmail.com>
2025-04-29 14:37:43 +1000

    add CarPage

--- src/pages/HistoryPage.tsx
+++ src/pages/HistoryPage.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useSearchParams } from 'react-router-dom';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import arrowDown from '@/images/arrow_down.svg';
